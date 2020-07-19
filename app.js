30a5291309fe544acd0f8872b6004a7f9f82e20c
unknown <alex@buildingbrandsmarketing.com>
2020-07-18 22:11:04 -0500

    first commit

--- app.js
+++ app.js
@@ -1,25 +1,153 @@
 
     // Create Dino Constructor
-
+    function Dino(species, weight, height, diet, where, when, fact) {
+        this.species = species;
+        this.weight = weight;
+        this.height = height;
+        this.diet = diet;
+        this.where = where;
+        this.when = when;
+        this.fact = fact;
+    }
 
     // Create Dino Objects
+    myDinos = [];
+
+    Dinos.map(dino => {
+        const oneDino = new Dino(
+            dino.species,
+            dino.weight,
+            dino.height,
+            dino.diet,
+            dino.where,
+            dino.when,
+            dino.fact
+        );
+        myDinos.push(oneDino);
+    });
+    console.log(myDinos[3].species);
 
 
     // Create Human Object
+    function Human (name, weight, height, diet) {
+        return {
+            name: name,
+            weight: weight,
+            height: height,
+            diet: diet
+        };
+    }
 
     // Use IIFE to get human data from form
 
+    // Make user variable global
+    let user;
 
-    // Create Dino Compare Method 1
-    // NOTE: Weight in JSON file is in lbs, height in inches. 
+    document.getElementById('btn').addEventListener('click', (event) => {
+        const human = (function () {
 
+            let name = document.getElementById('name').value;
+            let feet = document.getElementById("feet").value;
+            let inches = document.getElementById("inches").value;
+            let weight = document.getElementById('weight').value;
+            let diet = document.getElementById('diet').value;
     
-    // Create Dino Compare Method 2
-    // NOTE: Weight in JSON file is in lbs, height in inches.
+            function getName() {
+                return name;
+            }
+            function getHeight() {
+                feet = Number(feet);
+                inches = Number(inches);
+                finalHeight = ( feet * 12 ) + inches;
+                return finalHeight;
+            }
+            function getWeight() {
+                return weight;
+            }
+            function getDiet() {
+                return diet;
+            }
+            return {
+                name: getName(),
+                height: getHeight(),
+                weight: getWeight(),
+                diet: getDiet()
+            };
+        })();
 
+        // Creating a new human
+        user = Human (human.name, human.weight, human.height, human.diet);
+        console.log(user);
+
+        // Invoking the function to prepare the infographic
+        prepare();
+        compare2()
+    });
+
+    // Create Dino Compare Method 1 
+    function compare1 () {
+        myDinos.map(dino => {
+            // Get Weight, Height, and Diet to compare & randomize the outcode
+            let dinoArray = Object.keys(dino);
+            let dinoFacts = [dinoArray[1], dinoArray[2], dinoArray[3]];
+            let randomValue = dinoFacts[Math.floor(dinoFacts.length * Math.random())];
+
+            if(randomValue === "weight") {
+                if (dino.weight > user.weight) {
+                    let dinoWeighsMore = dino.weight - user.weight;
+                    console.log(`The ${dino.species} weighs ${dinoWeighsMore} lbs. more than ${user.name}!`);
+                } else {
+                    let humanWeighsMore = user.weight - dino.weight;
+                    console.log(`${user.name} weighs ${humanWeighsMore} lbs. more than the ${dino.species}!`);
+                }
+            } else if (randomValue === "height") {
+                if (dino.height > user.height) {
+                    let dinoIsTaller = dino.height - user.height;
+                    console.log(`The ${dino.species} is ${dinoIsTaller} inches taller than the ${user.name}!`);
+                } else {
+                    let humanIsTaller = user.height - dino.height;
+                    console.log(`The ${user.name} is ${humanIsTaller} inches taller than the ${dino.species}!`);
+                }
+            } else if (randomValue === "diet") {
+                let dinosDiet = dino.diet.charAt(0).toUpperCase() + dino.diet.slice(1)
+                if (dinosDiet === user.diet) {
+                    console.log(`The ${dino.species} and ${user.name} have the same diet!`);
+                } else {
+                    console.log(`The ${user.name} is a ${user.diet} while the ${dino.species} is a ${dino.diet}`);
+                }
+            }
+        });
+    }
+
+    // Create Dino Compare Method 2
+    function compare2 () {
+        myDinos.map(dino => {
+            // Get Weight, Height, and Diet to compare & randomize the outcode
+            let dinoArray = Object.keys(dino);
+            let dinoFacts = [dinoArray[1], dinoArray[2], dinoArray[3]];
+            let randomValue = dinoFacts[Math.floor(dinoFacts.length * Math.random())];
+
+            randomValue === "weight" ? console.log(`The ${dino.species} is ${dino.weight} lbs. and ${user.name} is ${user.weight} lbs.`)
+            : randomValue === "height" ? console.log(`The ${dino.species} is ${dino.height} inches and ${user.name} is ${user.height} inches`)
+            : randomValue === "diet" ? console.log(`The ${dino.species} is a ${dino.diet} and ${user.name} is a ${user.diet}`)
+            : console.log('Something broke!');
+
+        })
+    }
+      
     
     // Create Dino Compare Method 3
-    // NOTE: Weight in JSON file is in lbs, height in inches.
+    function compare3 () {
+        myDinos.map(dino => {
+
+            let dinoArray = Object.keys(dino);
+            let dinoFacts = [dinoArray[1], dinoArray[2], dinoArray[3]];
+            let randomValue = dinoFacts[Math.floor(dinoFacts.length * Math.random())];
+
+            
+
+        })
+    }
 
 
     // Generate Tiles for each Dino in Array
@@ -30,3 +158,8 @@
 
 
 // On button click, prepare and display infographic
+function prepare() {
+
+
+
+}
