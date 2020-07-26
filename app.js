b322fbd054386b1c306f5ff7e8f02f4a91939195
unknown <alexrich700@gmail.com>
2020-07-25 20:38:12 -0500

    Finalized project functionality

--- app.js
+++ app.js
@@ -25,7 +25,6 @@
         );
         myDinos.push(oneDino);
     });
-    console.log(myDinos[3].species);
 
 
     // Create Human Object
@@ -77,12 +76,32 @@
 
         // Creating a new human
         user = Human (human.name, human.weight, human.height, human.diet);
-        console.log(user);
 
         // Invoking the function to prepare the infographic
         prepare();
-        compare3()
     });
+    
+
+    // Random fact constructor. Used to store the random facts so that I can loop through and display them.
+    function DinosInformation(species, image, fact) {
+        this.species = species;
+        this.image = image;
+        this.fact = fact;
+    }
+    myGridInfo = [];
+
+    function createNewDinoItem (species, fact) {
+        let speciesLowerCase = species.charAt(0).toLowerCase() + species.slice(1);
+        let image = `images/${speciesLowerCase}.png`;
+        let dinosFact = species === "Pigeon" ? Dinos[7].fact : fact;
+
+        const oneFact = new DinosInformation(
+            species,
+            image,
+            dinosFact
+        );
+        myGridInfo.push(oneFact);
+    }
 
     // Create Dino Compare Method 1 
     function compare1 () {
@@ -95,25 +114,31 @@
             if(randomValue === "weight") {
                 if (dino.weight > user.weight) {
                     let dinoWeighsMore = dino.weight - user.weight;
-                    console.log(`The ${dino.species} weighs ${dinoWeighsMore} lbs. more than ${user.name}!`);
+                    let fact = `The ${dino.species} weighs ${dinoWeighsMore} lbs. more than ${user.name}!`;
+                    createNewDinoItem (dino.species, fact);
                 } else {
                     let humanWeighsMore = user.weight - dino.weight;
-                    console.log(`${user.name} weighs ${humanWeighsMore} lbs. more than the ${dino.species}!`);
+                    let fact = `${user.name} weighs ${humanWeighsMore} lbs. more than the ${dino.species}!`;
+                    createNewDinoItem (dino.species, fact);
                 }
             } else if (randomValue === "height") {
                 if (dino.height > user.height) {
                     let dinoIsTaller = dino.height - user.height;
-                    console.log(`The ${dino.species} is ${dinoIsTaller} inches taller than the ${user.name}!`);
+                    let fact = `The ${dino.species} is ${dinoIsTaller} inches taller than the ${user.name}!`;
+                    createNewDinoItem (dino.species, fact);
                 } else {
                     let humanIsTaller = user.height - dino.height;
-                    console.log(`The ${user.name} is ${humanIsTaller} inches taller than the ${dino.species}!`);
+                    let fact = `The ${user.name} is ${humanIsTaller} inches taller than the ${dino.species}!`;
+                    createNewDinoItem (dino.species, fact);
                 }
             } else if (randomValue === "diet") {
                 let dinosDiet = dino.diet.charAt(0).toUpperCase() + dino.diet.slice(1)
                 if (dinosDiet === user.diet) {
-                    console.log(`The ${dino.species} and ${user.name} have the same diet!`);
+                    let fact = `The ${dino.species} and ${user.name} have the same diet!`;
+                    createNewDinoItem (dino.species, fact);
                 } else {
-                    console.log(`The ${user.name} is a ${user.diet} while the ${dino.species} is a ${dino.diet}`);
+                    let fact = `The ${user.name} is a ${user.diet} while the ${dino.species} is a ${dino.diet}`;
+                    createNewDinoItem (dino.species, fact);
                 }
             }
         });
@@ -127,12 +152,12 @@
             let dinoFacts = [dinoArray[1], dinoArray[2], dinoArray[3]];
             let randomValue = dinoFacts[Math.floor(dinoFacts.length * Math.random())];
 
-            randomValue === "weight" ? console.log(`The ${dino.species} is ${dino.weight} lbs. and ${user.name} is ${user.weight} lbs.`)
-            : randomValue === "height" ? console.log(`The ${dino.species} is ${dino.height} inches and ${user.name} is ${user.height} inches`)
-            : randomValue === "diet" ? console.log(`The ${dino.species} is a ${dino.diet} and ${user.name} is a ${user.diet}`)
+            let fact = randomValue === "weight" ? `The ${dino.species} is ${dino.weight} lbs. and ${user.name} is ${user.weight} lbs.`
+            : randomValue === "height" ? `The ${dino.species} is ${dino.height} inches and ${user.name} is ${user.height} inches`
+            : randomValue === "diet" ? `The ${dino.species} is a ${dino.diet} and ${user.name} is a ${user.diet}`
             : console.log('Something broke!');
-
-        })
+            createNewDinoItem (dino.species, fact);
+        });
     }
       
     
@@ -148,18 +173,22 @@
                 let weighsMore;
                 let winner;
                 dino.weight > user.weight ? (weighsMore=dino.weight-user.weight, winner="dino") : (weighsMore=user.weight-dino.weight, winner="user");
-                winner === "dino" ? (console.log(`The ${dino.species} weighs ${weighsMore} lbs. more than ${user.name}!`)) : (console.log(`${user.name} weighs ${weighsMore} lbs. more than the ${dino.species}!`));
+                let fact = winner === "dino" ? `The ${dino.species} weighs ${weighsMore} lbs. more than ${user.name}!` : `${user.name} weighs ${weighsMore} lbs. more than the ${dino.species}!`;
+                createNewDinoItem (dino.species, fact);
             }
 
             function heightCalled () {
                 let isTaller;
                 let winner;
                 dino.height > user.height ? (isTaller=dino.height-user.height, winner="dino") : (isTaller=user.height-dino.height, winner="user");
-                winner === "dino" ? (console.log(`The ${dino.species} is ${isTaller} inches taller than ${user.name}!`)) : (console.log(`${user.name} weighs ${isTaller} inches taller than the ${dino.species}!`));
+                let fact = winner === "dino" ? `The ${dino.species} is ${isTaller} inches taller than ${user.name}!` : `${user.name} is ${isTaller} inches taller than the ${dino.species}!`;
+                createNewDinoItem (dino.species, fact);
             }
 
             function dietCalled () {
-                dino.diet === user.diet ? (console.log(`The ${dino.species} and ${user.name} have the same diet!`)) : (console.log(`The ${user.name} is a ${user.diet} while the ${dino.species} is a ${dino.diet}`));
+                let dinosDiet = dino.diet.charAt(0).toUpperCase() + dino.diet.slice(1)
+                let fact = dinosDiet === user.diet ? `The ${dino.species} and ${user.name} have the same diet!` : `${user.name} is a ${user.diet} while the ${dino.species} is a ${dino.diet}`;
+                createNewDinoItem (dino.species, fact);
             }
 
             switch (randomValue) {
@@ -176,20 +205,56 @@
                     console.log("Something broke!");
             }
 
-        })
+        });
+    }
+
+    // Suffles the grid array
+    function shuffleGrid(myGridInfo) {
+        for (let i = myGridInfo.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [myGridInfo[i], myGridInfo[j]] = [myGridInfo[j], myGridInfo[i]];
+        }
+        addHumanToGrid ();
+    }
+    // Adds a human to the grid array
+    function addHumanToGrid () {
+        myGridInfo.splice(4,0,user);
+    }
+
+    // Generate Tiles for each Dino in Array
+    function createGrid () {
+        const myGrid = document.getElementById("grid");
+        // Suffles the grid array and then calls addHumanToGrid
+        shuffleGrid(myGridInfo);
+
+        // Add tiles to DOM
+        myGridInfo.map(grid => {
+            function displayDino () {
+                const gridCode = `<div class="grid-item"><h3>${grid.species}</h3><img src="${grid.image}"><p>${grid.fact}</p></div>`;
+                myGrid.innerHTML += gridCode;
+            }
+            function displayHuman () {
+                const gridCode = `<div class="grid-item"><h3>${grid.name}</h3><img src="images/human.png"></div>`;
+                myGrid.innerHTML += gridCode;
+            }
+
+            // Checks if the item is a human
+            grid.name === user.name ? displayHuman()
+            : displayDino();
+        });
+
+        // Remove form from screen
+        (function () {
+            let myForm = document.getElementById('dino-compare');
+            myForm.style.display = 'none';
+        })();
+
     }
 
 
-    // Generate Tiles for each Dino in Array
-  
-        // Add tiles to DOM
-
-    // Remove form from screen
-
 
 // On button click, prepare and display infographic
-function prepare() {
-
-
-
+function prepare () {
+    compare3();
+    createGrid();
 }
